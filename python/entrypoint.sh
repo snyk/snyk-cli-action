ca0f4c11522f149a2d2ee19a216e85511b645830
Nick Doyle <nick.doyle@intelematics.com>
2019-09-17 11:28:51 +1000

    feat: python entrypoint pip install

--- python/entrypoint.sh
+++ python/entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/sh -l
 set -eu
+
+# Install all the requirements
+if [ -f $2 ] ; then
+	# If file specified, try getting requirements.txt from its directory
+	findfrom=`dirname $2`
+else
+	# Otherwise install from all requirements.txt in target path
+	findfrom=$2
+fi
+find $findfrom -name requirements.txt -exec bash -c 'pip install -r {}' \;
+
 sh -c "/usr/bin/snyk $*"
